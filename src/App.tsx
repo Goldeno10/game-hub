bd6c2adf9bc5670503403d95e5502eb737f7be2e
Goldeno10 <ibrahimmuhammad271@gmail.com>
2024-11-16 17:00:40 +0100

    Built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import './App.css'
-
-import { Button } from "@chakra-ui/react"
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-
   return (
-    <Button colorScheme="blue">Button</Button>
-  )
+    <Grid
+      templateAreas={{
+        base: `
+        "nav"
+        "main"
+      `,
+        lg: `
+        "nav nav"
+        "aside main"
+      `,
+      }}
+    >
+      <GridItem area={"nav"} bg="coral">
+        Nav
+      </GridItem>
+      {useBreakpointValue({
+        base: null,
+        lg: (
+          <GridItem area={"aside"} bg="gold">
+            Aside
+          </GridItem>
+        ),
+      })}
+      <GridItem area={"main"} bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
